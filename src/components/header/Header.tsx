314330f4080efd932bb95e213d75537213586dcf
Konstantin <timokhin_74@hotmail.com>
2020-10-13 16:29:43 +0500

    1st commit (Add header|profile|navbar|footer)

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Header.module.css'
+
+export function Header() {
+    return (
+        <div className={s.header}>
+            <img src="https://image.flaticon.com/icons/png/512/25/25719.png"/>
+        </div>
+    )
+}
