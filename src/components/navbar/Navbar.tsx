7cf98bff3a2bd1db2e484f7fcc54c9ddf4a358f6
Konstantin-174 <timokhin_74@hotmail.com>
2021-03-22 21:53:03 +0500

    26th commit (Trying to restyle)

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,22 +1,53 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 export function Navbar() {
     return (
         <nav className="section-inner section-navbar">
-            <div>
-                <NavLink to="/profile" className="section-navbar__active">Profile</NavLink>
-            </div>
-            <div>
-                {/* Задача NavLink без перезагрузки поменять url в адресной строке браузера (аналог тега </a>) */}
-                <NavLink to="/dialogs" className="section-navbar__active">Messages</NavLink>
-            </div>
-            <div>
-                <a href="#">News</a>
-            </div>
-            <div>
-                <a href="#">Friends</a>
-            </div>
+            <ul className="nav-wrapper">
+                <li className="nav-wrapper__item">
+                    <div className="nav-wrapper__item-link">
+                        {/* Задача NavLink без перезагрузки поменять url в адресной строке браузера (аналог тега </a>) */}
+                        <NavLink to="/profile" className="section-navbar__active">
+                            <span className="nav-wrapper__item-icon">
+                                icon
+                            </span>
+                            Profile
+                        </NavLink>
+                    </div>
+                </li>
+                <li className="nav-wrapper__item">
+                    <div className="nav-wrapper__item-link">
+                        <NavLink to="/dialogs" className="section-navbar__active">
+                            <span className="nav-wrapper__item-icon">
+                                icon
+                            </span>
+                            Messages
+                        </NavLink>
+                    </div>
+                </li>
+                <li className="nav-wrapper__item">
+                    <div className="nav-wrapper__item-link">
+                        <a href="#">
+                            <span className="nav-wrapper__item-icon">
+                                icon
+                            </span>
+                            News
+                        </a>
+                    </div>
+                </li>
+                <li className="nav-wrapper__item">
+
+                    <div className="nav-wrapper__item-link">
+                        <a href="#">
+                            <span className="nav-wrapper__item-icon">
+                                icon
+                            </span>
+                            Friends
+                        </a>
+                    </div>
+                </li>
+            </ul>
         </nav>
     )
 }
