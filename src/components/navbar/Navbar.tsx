e2c2e191a1e2070e446a04c7feed7f68d0e2b64f
Konstantin-174 <timokhin_74@hotmail.com>
2021-04-22 22:31:45 +0500

    1st (Added icons)

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,22 +1,36 @@
 import React from 'react';
-import local from './Navbar.module.scss'
+import local from './Navbar.module.scss';
+import { BsFillGrid1X2Fill, BsFillChatFill,
+    BsPeopleFill, BsFillExclamationSquareFill } from "react-icons/bs";
 
 const Navbar = () => {
     return (
         <section className={local.navWrap}>
             <div className={local.navContent}>
-                <div className={local.navItem}>
-                    Profile
-                </div>
-                <div className={local.navItem}>
-                    Messages
-                </div>
-                <div className={local.navItem}>
-                    Friends
-                </div>
-                <div className={local.navItem}>
-                    News
-                </div>
+                <a href="/profile" className={local.link}>
+                    <BsFillGrid1X2Fill className={local.navIcon}/>
+                    <div  className={local.navText}>
+                        Profile
+                    </div>
+                </a>
+                <a href="/messages" className={local.link}>
+                    <BsFillChatFill className={local.navIcon}/>
+                    <div className={local.navText}>
+                        Messages
+                    </div>
+                </a>
+                <a href="/friends" className={local.link}>
+                    <BsPeopleFill className={local.navIcon}/>
+                    <div className={local.navText}>
+                        Friends
+                    </div>
+                </a>
+                <a href="/news" className={local.link}>
+                    <BsFillExclamationSquareFill className={local.navIcon}/>
+                    <div className={local.navText}>
+                        News
+                    </div>
+                </a>
 
             </div>
         </section>
